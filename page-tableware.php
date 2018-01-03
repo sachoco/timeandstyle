ea218bffd1c4030729270d0e5e411d3a034be86e
Satoshi Shiraishi <sachoco@gmail.com>
2018-01-03 22:55:26 +0100

    wc customise

--- page-tableware.php
+++ page-tableware.php
@@ -58,10 +58,10 @@ foreach($product_categories as $cat):
                 ?>
                 <li class="series-item" id="<?php echo esc_attr($loop->post->post_name ? $loop->post->post_name : $loop->post->ID); ?>">
                     <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>" data-id="<?php echo $loop->post->ID; ?>">
-                        <div class="image-wrapper">
+                        <div class="">
                         <?php woocommerce_show_product_sale_flash( $post, $product ); ?>
 
-                        <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'full'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
+                        <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
                         </div>
                         <h3><?php the_title(); ?></h3>
 
