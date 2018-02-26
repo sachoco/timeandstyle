16d1676002e70a21d3a7a263d9ca3f93cbb33754
Satoshi Shiraishi <sachoco@gmail.com>
2018-02-26 23:39:08 +0100

    disable "add to cart"

--- functions.php
+++ functions.php
@@ -305,8 +305,8 @@ remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_p
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 
-add_action( 'woocommerce_after_single_product_summary', 'woocommerce_template_single_price', 10 );
-add_action( 'woocommerce_after_single_product_summary', 'woocommerce_template_single_add_to_cart', 15 );
+// add_action( 'woocommerce_after_single_product_summary', 'woocommerce_template_single_price', 10 );
+// add_action( 'woocommerce_after_single_product_summary', 'woocommerce_template_single_add_to_cart', 15 );
 
 /**
  * Ensure cart contents update when products are added to the cart via AJAX
