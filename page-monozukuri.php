42bea2af2959c230cdcd68659989bd1095fce01c
Satoshi Shiraishi <sachoco@gmail.com>
2019-06-06 22:43:14 +0200

    Monozukuri movie

--- page-monozukuri.php
+++ page-monozukuri.php
@@ -19,7 +19,7 @@ foreach($categories as $cat):
         $loop = new WP_Query( $args );
         while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
 
-                <li class="product">    
+                <li class="product">
 
                     <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
                         <div class="image-wrapper">
@@ -59,13 +59,13 @@ foreach($categories as $cat):
             <div class="icon">
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
      width="31.131px" height="17.097px" viewBox="0 0 31.131 17.097" enable-background="new 0 0 31.131 17.097" xml:space="preserve">
-<polyline id="line-1" fill="none" stroke="#FFFFFF" stroke-width="1" stroke-miterlimit="10" points="30.581,16.369 15.715,1.503 0.848,16.369 
+<polyline id="line-1" fill="none" stroke="#FFFFFF" stroke-width="1" stroke-miterlimit="10" points="30.581,16.369 15.715,1.503 0.848,16.369
     "/>
 </svg>
             </div>
-            <div class="bg circular-anim"></div>  
+            <div class="bg circular-anim"></div>
         </div>
-                                    
+
     </div>
 </div>
 <div id="opening">
@@ -94,22 +94,22 @@ foreach($categories as $cat):
         <div class="image-wrapper">
             <img src="<?php echo get_template_directory_uri(); ?>/images/monozukuri/2.MZ.jpg" >
         </div>
-    </li>   
+    </li>
     <li>
         <div class="image-wrapper">
             <img src="<?php echo get_template_directory_uri(); ?>/images/monozukuri/3.MZ.jpg" >
         </div>
-    </li>  
+    </li>
     <li>
         <div class="image-wrapper">
             <img src="<?php echo get_template_directory_uri(); ?>/images/monozukuri/4.MZ.jpg" >
         </div>
-    </li> 
+    </li>
     <li>
         <div class="image-wrapper">
             <img src="<?php echo get_template_directory_uri(); ?>/images/monozukuri/5.MZ.jpg" >
         </div>
-    </li> 
+    </li>
    	<li class="footer">
     	<footer class="footer">
             <?php wp_nav_menu( array( 'menu' => 'Footer Menu') ); ?>
@@ -126,13 +126,13 @@ foreach($categories as $cat):
 		    <div class="icon">
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 width="31.131px" height="17.097px" viewBox="0 0 31.131 17.097" enable-background="new 0 0 31.131 17.097" xml:space="preserve">
-<polyline id="line-1" fill="none" stroke="#FFFFFF" stroke-width="1" stroke-miterlimit="10" points="30.581,16.369 15.715,1.503 0.848,16.369 
+<polyline id="line-1" fill="none" stroke="#FFFFFF" stroke-width="1" stroke-miterlimit="10" points="30.581,16.369 15.715,1.503 0.848,16.369
 	"/>
 </svg>
 		    </div>
-		    <div class="bg circular-anim"></div>  
+		    <div class="bg circular-anim"></div>
 		</div>
-		                            
+
 	</div>
 </div>
 
